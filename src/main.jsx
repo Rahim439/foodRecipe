a36411d7facc3c3eada94dcbcc1fa9bb344b721a
Rahim <rahimch439@gmail.com>
2024-07-16 01:15:10 +0500

    Deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
-import BrowserRouter from "react-router-dom/BrowserRouter";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
